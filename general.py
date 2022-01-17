5ab26a2ef0df800942f26e8fba8cb9b587b86f02
m-henke <mthenke2000@gmail.com>
2022-01-17 01:18:16 -0500

    class change

--- general.py
+++ general.py
@@ -1,24 +1,23 @@
 class Values:
-    def __init__(self):
-        self.WIDTH = 1000
-        self.HEIGHT = 600
+    WIDTH = 1000
+    HEIGHT = 600
 
-        self.TOP_HEIGHT = 40  # height of the top input box
+    TOP_HEIGHT = 40  # height of the top input box
 
-        self.CANVAS_HEIGHT = self.HEIGHT - self.TOP_HEIGHT  # height of the canvas area
-        self.MAX_DATA_SIZE = 500  # max number of data elements
-        self.DEFAULT_DATA_SIZE = 100  # default number of data elements
-        self.ADJUSTER = self.WIDTH//self.DEFAULT_DATA_SIZE  # number to adjust the width of a line
+    CANVAS_HEIGHT = HEIGHT - TOP_HEIGHT  # height of the canvas area
+    MAX_DATA_SIZE = 500  # max number of data elements
+    DEFAULT_DATA_SIZE = 100  # default number of data elements
+    ADJUSTER = WIDTH//DEFAULT_DATA_SIZE  # number to adjust the width of a line
 
-        self.SPEEDS = ['Slow', 'Medium', 'Fast']
-        self.ALGORITHMS = [
-            'Bitonic Sort',
-            'Bubble Sort',
-            'Cocktail Shaker Sort',
-            'Insertion Sort',
-            'Merge Sort',
-            'Quick Sort'
-        ]
+    SPEEDS = ['Slow', 'Medium', 'Fast']
+    ALGORITHMS = [
+        'Bitonic Sort',
+        'Bubble Sort',
+        'Cocktail Shaker Sort',
+        'Insertion Sort',
+        'Merge Sort',
+        'Quick Sort'
+    ]
 
 
 def swap(data, x, y):
